d435be49c14093cb34987631d67f685b8e54eb86
Brian Ababu <brianababu63@gmail.com>
2023-05-19 22:19:39 +0300

    added a muted text to the title 🎀

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,14 @@ import {Link} from 'react-router-dom';
 const Navbar=()=>{
     return(
         <nav className="navbar">
-        <h2>TodoApp...</h2>
+            <h2>TodoApp  <br/> <h5>Keep Track of your day✨</h5> </h2>
+           
             <div className="links">
             
                 <Link to="/">Home</Link>
                 <Link to="/TodoForm">Add Item</Link>
             </div>
-    </nav>
+        </nav>
     )
 }
 
